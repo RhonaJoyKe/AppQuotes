8379932484c3ad42f0a5cf69731b18dcd0023f44
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-11 01:52:45 +0300

    modifies the delete button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,9 +8,9 @@ import { Quotes} from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   quote:Quotes[] = [
-    new Quotes(1, 'Sansa Stark', 'The lone wolf dies but the pack survives','Rhonajoy',new Date(2021,10,4)),
-    new Quotes(2,'Aria Stark','The lone wolf dies but the pack survives ','Rhonajoy',new Date(2021,10,4)),
-    new Quotes(3,'Jon Snow','The lone wolf dies but the pack survives','Rhonajoy',new Date(2021,10,4)),
+    new Quotes(1, 'Sansa Stark','Jojo','The lone wolf dies but the pack survives',new Date(2021,10,4)),
+    new Quotes(2,'Aria Stark','Bre','The lone wolf dies but the pack survives ',new Date(2021,10,4)),
+    new Quotes(3,'Jon Snow','Dennis','The lone wolf dies but the pack survives',new Date(2021,10,4)),
   ];
   toggleDetails(index: number){
     this.quote[index].showauthor = !this.quote[index].showauthor;
